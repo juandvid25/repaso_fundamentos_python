7c67aca0d9bfa39e467caf7ceada50ffc17d256a
Juan.David.Mosquera.Palacios <juandavidmospa12@gmail.com>
2026-07-26 21:43:53 -0500

    ejercicios de la sesion 2, cadenas

---
+++ sesion 2/cadena.py
@@ -0,0 +1,8 @@
+# Programa: Uso de caracteres de escape en Python
+# Autor: Juan David
+# Descripción:
+# Este programa muestra cómo imprimir comillas dobles dentro de un texto
+# utilizando el carácter de escape (\").
+
+# Imprime la frase con comillas dobles incluidas en la salida.
+print("\"Estoy\"\"\"aprendiendo\"\"\"\"\"Python\"\"\"")
